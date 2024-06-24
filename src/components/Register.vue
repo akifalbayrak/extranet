50bf4362933dbcc13aa4e1b5eee03d03231e9c2e
akif albayrak <akifalbayrak6161@gmail.com>
2024-06-24 11:11:23 +0300

    refactor: Update Register.vue styling and layout
    The Register.vue component has been updated to improve its styling and layout. Changes include updating the background color, font sizes, and spacing to enhance the overall visual appeal of the component.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -10,7 +10,7 @@
                 to="/login"
                 tag="button"
                 type="button"
-                class="hover:bg-gray-700 text-white border border-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline transition duration-300 ease-in-out">
+                class="hover:bg-gray-500 text-white border border-white font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline transition duration-300 ease-in-out">
                 Oturum aç
             </router-link>
         </div>
@@ -30,7 +30,7 @@
         <RegisterCard />
     </div>
     <div
-        class="flex flex-col items-center justify-center bg-gray-100 px-80 pt-10">
+        class="flex flex-col items-center justify-center bg-gray-100 px-80 py-10">
         <h1 class="mb-6 text-3xl font-bold text-gray-700">
             Vihobook Dijital Marketing ile işbirliğiniz sayesinde daha kazançlı
             çıkın
@@ -105,6 +105,42 @@
                     yüksek doluluk oranı sağlamanıza yardımcı oluyoruz
                 </p>
             </div>
+        </div>
+    </div>
+    <div class="flex bg-white px-80 justify-between">
+        <div class="flex">
+            <span>Copyright ©️ 2024.</span>
+            <img
+                src="https://extranet.vihobook.com/static/media/vBlueLogo.703be7df.png"
+                class="w-auto h-6 mx-3"
+                alt="logo" />
+            <span>All rights reserved.</span>
+        </div>
+
+        <div class="flex">
+            <svg
+                class="w-5 h-5 mr-2 my-auto"
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 48 48">
+                <g
+                    fill="none"
+                    stroke="#000"
+                    stroke-linejoin="round"
+                    stroke-width="4">
+                    <path
+                        stroke-linecap="round"
+                        d="M41.7796 20.6066C42.0324 18.9108 41.9495 17.1747 41.5309 15.5054C40.978 13.3002 39.8392 11.2118 38.1147 9.4873C36.3902 7.76281 34.3018 6.62409 32.0967 6.07115C30.4274 5.65257 28.6912 5.56967 26.9954 5.82245" />
+                    <path
+                        stroke-linecap="round"
+                        d="M34.1897 19.8035C34.4605 17.9869 33.8967 16.0699 32.4983 14.6715C31.0998 13.2731 29.1829 12.7092 27.3663 12.98" />
+                    <path
+                        fill="#2f88ff"
+                        d="M14.3757 8.79424C15.1022 8.79424 15.7716 9.1883 16.1243 9.8235L18.5707 14.2303C18.891 14.8073 18.9061 15.5052 18.6109 16.0955L16.2541 20.8091C16.2541 20.8091 16.9371 24.3206 19.7955 27.179C22.654 30.0374 26.1536 30.7086 26.1536 30.7086L30.8665 28.3522C31.4572 28.0568 32.1556 28.0721 32.7328 28.393L37.1521 30.85C37.7868 31.2028 38.1803 31.8719 38.1803 32.598V37.6715C38.1803 40.2552 35.7804 42.1213 33.3323 41.2952C28.3044 39.5987 20.4997 36.3685 15.5529 31.4216C10.606 26.4748 7.37579 18.6701 5.67928 13.6422C4.85325 11.1941 6.71934 8.79424 9.30299 8.79424H14.3757Z" />
+                </g>
+            </svg>
+            <span> +90 850 888 7339 </span>
         </div>
     </div>
 </template>
--- src/components/RegisterCard.vue
+++ src/components/RegisterCard.vue
@@ -1,19 +1,84 @@
 <template>
-    <div class="bg-white rounded-md p-6 text-sm shadow-lg">
-        <h2 class="text-2xl font-bold text-gray-700 my-4">
-            Şimdi ücretsiz üye olun ve daha fazla kazanın!
+    <div class="bg-white rounded-md py-4 px-6 text-sm shadow-lg">
+        <h2 class="text-2xl font-bold text-gray-700 mt-2 mb-4">
+            Şimdi ücretsiz üye olun ve <br />
+            daha fazla kazanın!
         </h2>
         <p class="text-gray-600 my-4 font-semibold flex">
+            <svg
+                class="h-4 w-4 text-black mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 48 48">
+                <path
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="4"
+                    d="m4 24l5-5l10 10L39 9l5 5l-25 25z"
+                    clip-rule="evenodd" />
+            </svg>
             Affiliate marketing ile daha fazla etkileşim elde edin!
         </p>
-        <p class="text-gray-600 my-4 font-semibold">
+        <p class="text-gray-600 my-4 font-semibold flex">
+            <svg
+                class="h-4 w-4 text-black mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 48 48">
+                <path
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="4"
+                    d="m4 24l5-5l10 10L39 9l5 5l-25 25z"
+                    clip-rule="evenodd" />
+            </svg>
             Tüm Turizm Sektörüne hitap eden Turizm Dijital Pazarlama
         </p>
 
-        <p class="text-gray-600 my-4 font-semibold">
+        <p class="text-gray-600 my-4 font-semibold flex">
+            <svg
+                class="h-4 w-4 text-black mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 48 48">
+                <path
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="4"
+                    d="m4 24l5-5l10 10L39 9l5 5l-25 25z"
+                    clip-rule="evenodd" />
+            </svg>
             Vihobook'tan ödemeleri alma kolaylığı
         </p>
-        <p class="text-gray-600 my-4 font-semibold">
+        <p class="text-gray-600 my-4 font-semibold flex">
+            <svg
+                class="h-4 w-4 text-black mr-2"
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 48 48">
+                <path
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="4"
+                    d="m4 24l5-5l10 10L39 9l5 5l-25 25z"
+                    clip-rule="evenodd" />
+            </svg>
             Kayıt işlemi ücretsizdir ve 15 dakikanızı alır
         </p>
         <hr class="my-4" />
@@ -26,15 +91,17 @@
         </router-link>
 
         <hr class="my-4" />
-        <p class="text-gray-600">
-            Zaten hesabınız var mı?
-            <router-link
-                to="/login"
-                tag="button"
-                type="button"
-                class="text-indigo-500 hover:text-indigo-600">
-                Oturum aç
-            </router-link>
-        </p>
+        <div class="text-center">
+            <p class="text-gray-600 font-semibold">
+                Zaten hesabınız var mı?
+                <router-link
+                    to="/login"
+                    tag="button"
+                    type="button"
+                    class="text-indigo-500 hover:text-indigo-600">
+                    Oturum aç
+                </router-link>
+            </p>
+        </div>
     </div>
 </template>
